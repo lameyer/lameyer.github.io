98a50543e4132a8932f4a6a190f25a26fe439a7c
Lindsey <lameyer@users.noreply.github.com>
2015-06-06 12:35:35 -0700

    fix scroll fade

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var scrollDist = $(document).scrollTop();
     var transparency = .9;
-    if (scrollDist < 600) {
-      transparency = scrollDist / 540;
+    if (scrollDist <= 700) {
+      transparency = scrollDist / 770;
     }
     $("#header").css({
       "background": "rgba(255, 255, 255, " + transparency + ")"
