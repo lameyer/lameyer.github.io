64f67e230dd9e1c76332b75d9a6393aff7969b1c
Lindsey <lameyer@users.noreply.github.com>
2015-06-07 14:55:07 -0700

    clean up

--- js/scroll.js
+++ js/scroll.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
     if (scrollDist <= 700) {
       transparency = scrollDist / 770;
     }
-    $("#header").css({
+    $("header").css({
       "background": "rgba(255, 255, 255, " + transparency + ")"
     });
 
   });
 
   // animate scroll to anchor from nav bar
-  $("#header a").click(function() {
+  $("header a").click(function() {
     var anchor = $(this).attr("href")
     $('html,body').animate({scrollTop: $(anchor).offset().top},'slow');
     return false;
